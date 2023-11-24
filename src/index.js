631e229559ccf2a25b330babc13564c066ad9eb1
Carl <2009wpeng@gmail.com>
2023-11-24 19:48:37 +0800

    carl push

--- src/index.js
+++ src/index.js
@@ -4,6 +4,17 @@ import nuxt from './rules/nuxt.js'
 import vue from './rules/vue.js'
 import tailwindcss from './rules/tailwindcss.js'
 
+const woodawn = (...args) => {
+  return antfu(
+    {}, // This must be the first argument
+    // From the second arguments they are ESLint Flat Configs
+    // you can have multiple configs
+    base,
+    tailwindcss,
+    ...args,
+  )
+}
+
 const woovue = (...args) => {
   return antfu(
     {}, // This must be the first argument
@@ -30,6 +41,7 @@ const woonuxt = (...args) => {
 }
 
 export {
-  woovue,
-  woonuxt,
+  woodawn, // 基本项目
+  woovue, // vue 项目
+  woonuxt, // nuxt 项目
 }
