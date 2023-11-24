392a879aad66945d94b25290b60570f532ece79f
Carl <2009wpeng@gmail.com>
2023-11-24 15:58:37 +0800

    carl push

--- src/rules/base.js
+++ src/rules/base.js
@@ -2,7 +2,7 @@ export default [
   // General Rules
   {
     rules: {
-      'curly': ['error', 'all'],
+      'curly': ['error', 'all'], // 不省略大括号
       'style/function-call-spacing': ['error', 'never'],
       'node/prefer-global/process': 'off',
       'antfu/top-level-function': 'off',
